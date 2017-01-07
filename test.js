49e3126c1828b624dad4aa21535adee617bc307b
mpmckenna8 <mpmckenna8@me.com>
2017-01-06 16:50:38 -0800

    Got the basic test thing working to send an elephant and not have like a million extra comments and function calls.

--- test.js
+++ test.js
@@ -2,22 +2,23 @@ var imagemagick = require('imagemagick-native');
 var fs = require('fs')
 var nb = require('./index.js');
 var file = "./elephant.jpeg"
-nb.hostname = 'localhost'
+//nb.hostname = 'localhost'
 const footer = new Buffer(nb.footerString)
 
 var srcData = fs.readFileSync(file);
 
-nb.init();
 var content = imagemagick.convert({
   srcData: srcData,
   width: 45,
   height: 35,
   format: 'ppm'
 })
-//nb.init()
+
 var totlen = footer.length + content.length;
 
 var allcon = Buffer.concat([content, footer], totlen)
 
 nb.data = allcon;
+
+// Send the data to the flaschen-taschen
 nb.show()
